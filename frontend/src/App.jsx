9706e03cc2a22368f8d5b58191ec886aed8706d6
kekemon1230 <115252310+kekemon1230@users.noreply.github.com>
2025-04-07 18:40:59 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Tailwind 테스트!
+    </div>
+  );
+}
+
+export default App;
+
 // function App() {
 //   const [count, setCount] = useState(0)
 
@@ -34,13 +44,13 @@ import './App.css'
 
 // export default App
 
-function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold">Unknown to Everyone Knows</h1>
-      <p className="text-gray-700 mt-2">익명의 작품 투표 플랫폼</p>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+//       <h1 className="text-3xl font-bold">Unknown to Everyone Knows</h1>
+//       <p className="text-gray-700 mt-2">익명의 작품 투표 플랫폼</p>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;

6653432233a02693cf00ec19527bfb840d13f408
kekemon1230 <115252310+kekemon1230@users.noreply.github.com>
2025-04-07 18:45:44 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./index.html",  // ✅ HTML 파일 감지
+    "./src/**/*.{js,ts,jsx,tsx}" // ✅ JS/TS/JSX/TSX 파일 감지
   ],
   theme: {
     extend: {},

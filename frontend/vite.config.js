f339b3cfb8945893f592827843a0f2f1205b1a80
kekemon1230 <115252310+kekemon1230@users.noreply.github.com>
2025-04-07 18:10:34 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "./postcss.config.js",
-  },
   server: {
     host: true,
     port: 5173,

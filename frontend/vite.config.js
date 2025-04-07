1a8d4c8a11ab7d1d30af7a3ae7d5c678921aef65
kekemon1230 <115252310+kekemon1230@users.noreply.github.com>
2025-04-07 17:50:59 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
   server: {
     host: true,
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
